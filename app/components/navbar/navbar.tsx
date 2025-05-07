f0590ce770c28feaf154588c6b2829b64cdf7f56
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-08 00:44:06 +0700

    fix linting error : unused func in navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -5,14 +5,6 @@ import NavLinks from "./navlinks";
 import NavButtons from "./navbuttons";
 
 export default function Navbar() {
-  const getButtonClass = (type: string) =>
-    clsx("py-1 px-3 rounded-[6px] text-[14px]", {
-      "bg-white text-black font-semibold border border-white hover:bg-white-hover":
-        type === "primary",
-      "bg-block-gray text-white border border-stroke-gray hover:bg-block-gray-hover":
-        type === "secondary",
-    });
-
   return (
     <nav className="w-full h-16 border-b border-stroke-gray">
       <div className="flex justify-between items-center w-full h-full px-[80px]">

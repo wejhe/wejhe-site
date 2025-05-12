fec0da8e41b4b07a5e8dc147433b18a7f2b53999
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-12 23:48:37 +0700

    add outro section to landing page and implement its responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/landing-page/hero/hero";
 import TechStack from "@/app/components/landing-page/tech-stack/tech-stack";
+import Outro from "@/app/components/landing-page/outro";
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center gap-lp-section-padding py-lp-y-padding-mobile lg:py-lp-y-padding-desktop w-full">
       <Hero />
       <TechStack />
+      <Outro />
     </div>
   );
 }

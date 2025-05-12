834e95f888047ffdff25cc01ada467431b0b0eea
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-12 14:22:25 +0700

    implement responsive design on hero section

--- app/components/landing-page/hero/hero.tsx
+++ app/components/landing-page/hero/hero.tsx
@@ -1,19 +1,10 @@
 import HeroStatus from "@/app/components/landing-page/hero/hero-status";
 import { LargeButton } from "@/app/components/buttons";
 import { ButtonConfig } from "@/app/lib/definitions";
+import { heroStatuses, heroButtons } from "@/app/lib/ui-constraints";
 import Link from "next/link";
 
 export default function Hero() {
-  const statuses = [
-    { icon: "globe.svg", label: "Jakarta, Indonesia" },
-    { icon: "aperture.svg", label: "Available for Projects" },
-  ];
-
-  const buttons: ButtonConfig[] = [
-    { href: "/consult", type: "primary", label: "Consult a Project" },
-    { href: "/blog", type: "secondary", label: "Read Blog Posts" },
-  ];
-
   const description = (
     <>
       Crafting reliable, intuitive, and seamless{" "}
@@ -24,27 +15,27 @@ export default function Hero() {
   );
 
   return (
-    <div className="flex flex-col items-center gap-[64px] w-full px-body-padding-desktop">
+    <div className="flex flex-col items-center gap-[64px] w-full px-body-padding-mobile lg:px-body-padding-desktop">
       <div className="flex flex-col items-center gap-[40px] w-full">
-        <div className="flex items-center justify-center gap-[24px] w-full">
-          {statuses.map((status) => (
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-[16px] lg:gap-[24px] w-full">
+          {heroStatuses.map((status) => (
             <HeroStatus key={status.icon} icon={status.icon}>
               {status.label}
             </HeroStatus>
           ))}
         </div>
         <div className="flex flex-col items-center gap-[24px] w-full">
-          <h1 className="text-white text-center font-bold text-[64px] w-full">
+          <h1 className="text-white text-center font-bold text-[64px] leading-[80px] lg:leading-normal w-full">
             I&apos;m a Software Developer
           </h1>
-          <p className="text-textual-gray text-center leading-[40px] text-[24px] w-[780px]">
+          <p className="text-textual-gray text-center leading-[36px] lg:leading-[40px] text-[20px] lg:text-[24px] w-full lg:w-[780px]">
             {description}
           </p>
         </div>
       </div>
-      <div className="flex items-center justify-center gap-[24px] w-full">
-        {buttons.map((button) => (
-          <Link key={button.label} href={button.href}>
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-[16px] lg:gap-[24px] w-full">
+        {heroButtons.map((button) => (
+          <Link key={button.label} href={button.href} className="w-full">
             <LargeButton type={button.type}>{button.label}</LargeButton>
           </Link>
         ))}

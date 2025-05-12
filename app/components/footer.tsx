63241e758fdbfe065a58f5d342a915a22e50613c
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-12 22:04:41 +0700

    add footer component to base layout

---
+++ app/components/footer.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <nav className="bg-black w-full h-navbar-height border-t border-stroke-gray mt-auto">
+      <div className="flex justify-between items-center w-full h-full px-body-padding-mobile lg:px-body-padding-desktop">
+        <div className="flex gap-[4px] items-center justify-center">
+          <p className="text-[16px] text-textual-gray">Copyright</p>
+          <span className="text-[24px] text-textual-gray font-extralight">
+            ©
+          </span>
+          <p className="text-[16px] text-textual-gray">2025 Wahyu Wijiyanto</p>
+        </div>
+        <Link
+          key="Privacy Policy"
+          href="/privacy-policy"
+          className="text-[16px] text-textual-gray hover:text-link-blue"
+        >
+          Privacy Policy
+        </Link>
+      </div>
+    </nav>
+  );
+}

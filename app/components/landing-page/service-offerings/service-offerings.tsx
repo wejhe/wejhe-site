bc5ba7a619d24e22473fa3f629e7f0b2fe0a6f35
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-16 11:30:04 +0700

    implement responsive design on Service Offerings section

--- app/components/landing-page/service-offerings/service-offerings.tsx
+++ app/components/landing-page/service-offerings/service-offerings.tsx
@@ -6,14 +6,14 @@ import Link from "next/link";
 
 export default function ServiceOfferings() {
   return (
-    <div className="flex flex-col items-center gap-[64px] w-full">
-      <div className="px-body-padding-mobile lg:px-body-padding-desktop">
+    <div className="flex flex-col items-center gap-[64px] w-full px-body-padding-mobile lg:px-0">
+      <div className="px-0 lg:px-body-padding-desktop">
         <SectionTitle
           title="Service Offerings"
           subtitle="Exploring, shaping, and delivering digital solutions tailored to your unique needs"
         />
       </div>
-      <div className="flex flex-col lg:flex-row bg-block-gray border-y border-stroke-gray items-center justify-center gap-[40px] w-full py-[40px] px-body-padding-mobile lg:px-body-padding-desktop">
+      <div className="flex flex-col lg:flex-row bg-block-gray rounded-[24px] lg:rounded-[0px] border lg:border-y border-stroke-gray items-center justify-center lg:items-start gap-[64px] lg:gap-[40px] w-full py-[40px] px-body-padding-mobile lg:px-body-padding-desktop">
         {serviceOfferings.map((service) => (
           <ServiceCard
             key={service.title}

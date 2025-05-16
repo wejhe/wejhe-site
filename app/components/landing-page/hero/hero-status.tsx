b1a732ba7f04f25814d15c0c39f61053666b0363
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-16 22:16:47 +0700

    minor change on Hero Status (padding-right)

--- app/components/landing-page/hero/hero-status.tsx
+++ app/components/landing-page/hero/hero-status.tsx
@@ -8,7 +8,7 @@ export default function HeroStatus({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex items-center gap-[12px] py-[8px] pl-[12px] pr-[20px] rounded-[100px] bg-block-gray border border-stroke-gray text-white text-[14px]">
+    <div className="flex items-center gap-[12px] py-[8px] pl-[12px] pr-[18px] rounded-[100px] bg-block-gray border border-stroke-gray text-white text-[14px]">
       <Image src={`/${icon}`} width={24} height={24} alt={icon} />
       <p className="text-center">{children}</p>
     </div>

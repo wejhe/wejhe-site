790901dbdf57ec66d8bd76858a7ac71162271eab
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2025-05-18 18:04:25 +0700

    visual improvement on Tech Stack section (add background image)

--- app/components/landing-page/tech-stack/tech-card.tsx
+++ app/components/landing-page/tech-stack/tech-card.tsx
@@ -13,12 +13,12 @@ export default function TechCard({
 }) {
   return (
     <div className="bg-black border border-stroke-gray rounded-[24px] p-[8px] w-full">
-      <div className="bg-gradient-to-b from-block-gray to-black flex flex-col items-center justify-center rounded-[16px] gap-[40px] py-[40px] w-full">
-        <div className="flex flex-col text-center items-center justify-center gap-[4px] w-full">
+      <div className="relative bg-gradient-to-b from-block-gray to-black flex flex-col items-center justify-center rounded-[16px] gap-[40px] py-[40px] w-full">
+        <div className="flex flex-col text-center items-center justify-center gap-[4px] w-full z-2">
           <strong className="text-white text-[24px] font-bold">{title}</strong>
           <p className="text-textual-gray text-[20px]">{subtitle}</p>
         </div>
-        <div className="flex flex-col text-center items-center justify-center w-full">
+        <div className="flex flex-col text-center items-center justify-center w-full z-2">
           <Image
             src={`/${firstImage}`}
             width={240}
@@ -32,6 +32,13 @@ export default function TechCard({
             alt={secondImage}
           />
         </div>
+        <Image
+          src="/bg-globe.png"
+          width={360}
+          height={360}
+          alt="Globe Background"
+          className="absolute bottom-[-8px] z-1"
+        />
       </div>
     </div>
   );
